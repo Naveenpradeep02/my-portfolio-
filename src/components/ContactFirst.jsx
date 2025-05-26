ea2d6268406cdc38d5c439b7fe281c78c2a7152d
naveenpradeep0987@gmail.com <Naveenpradeep02.noreply.github.com>
2025-05-26 14:41:53 +0530

    update

--- src/components/ContactFirst.jsx
+++ src/components/ContactFirst.jsx
@@ -92,7 +92,7 @@ const ContactFirst = () => {
                 </div>
               </motion.div>
             </a>
-            <a>
+            <a href="">
               <motion.div
                 initial={{ opacity: 0, x: 100 }}
                 whileInView={{ opacity: 1, x: 0 }}
